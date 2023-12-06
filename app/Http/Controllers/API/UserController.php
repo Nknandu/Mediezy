c31f6b662f3d2545539edcb43de0d1e7fc0f4972
PriyaK <115984677+PriyaSKumarrm@users.noreply.github.com>
2023-12-06 10:40:42 +0530

    changes 06-12-23 10:40

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -162,26 +162,26 @@ class UserController extends BaseController
     }
 
 
-    public function UserLogin(Request $req)
-    {
-        // validate inputs
-        $rules = [
-            'email' => 'required',
-            'password' => 'required|string'
-        ];
-        $req->validate($rules);
-        // find user email in users table
-        $user = User::where('email', $req->email)->first();
+    // public function UserLogin(Request $req)
+    // {
+    //     // validate inputs
+    //     $rules = [
+    //         'email' => 'required',
+    //         'password' => 'required|string'
+    //     ];
+    //     $req->validate($rules);
+    //     // find user email in users table
+    //     $user = User::where('email', $req->email)->first();
 
-        // if user email found and password is correct
-        if ($user && Hash::check($req->password, $user->password)) {
-            $token = $user->createToken('Personal Access Token')->plainTextToken;
-            $response = ['user' => $user, 'token' => $token];
-            return response()->json($response, 200);
-        }
-        $response = ['message' => 'Incorrect email or password'];
-        return response()->json($response, 400);
-    }
+    //     // if user email found and password is correct
+    //     if ($user && Hash::check($req->password, $user->password)) {
+    //         $token = $user->createToken('Personal Access Token')->plainTextToken;
+    //         $response = ['user' => $user, 'token' => $token];
+    //         return response()->json($response, 200);
+    //     }
+    //     $response = ['message' => 'Incorrect email or password'];
+    //     return response()->json($response, 400);
+    // }
 
 
     private function getClinics($doctorId)
